0954872eb6dd312e54e0acd4deab05e722918bd4
Jose Ortega <jo.manuel.ortega@gmail.com>
2025-06-27 22:05:11 -0600

    Fix env file

--- api/config.py
+++ api/config.py
@@ -3,8 +3,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "Stories API"
-    # openai_key: str
+
+    openai_key: str | None = None
     openai_model: str = "gpt-3.5-turbo"
+
+    gemini_key: str | None = None
+    gemini_model: str = "gemini-2.5-flash"
+
     model_config = SettingsConfigDict(env_file=".env")
 
 
